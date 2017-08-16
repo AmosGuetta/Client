1912b2d5070d8b77b3c7dbafee27fece24171444
AmosGuetta <amos.guetta.1990@gmail.com>
2017-08-16 17:42:28 +0300

    Fixed git ignore

--- cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/amos/Client/src/EncoderDecoder.cpp" "/Users/amos/Client/cmake-build-debug/CMakeFiles/Client.dir/src/EncoderDecoder.cpp.o"
-  "/Users/amos/Client/src/client.cpp" "/Users/amos/Client/cmake-build-debug/CMakeFiles/Client.dir/src/client.cpp.o"
-  "/Users/amos/Client/src/connectionHandler.cpp" "/Users/amos/Client/cmake-build-debug/CMakeFiles/Client.dir/src/connectionHandler.cpp.o"
+  "/Users/amos/Development/Client/src/EncoderDecoder.cpp" "/Users/amos/Development/Client/cmake-build-debug/CMakeFiles/Client.dir/src/EncoderDecoder.cpp.o"
+  "/Users/amos/Development/Client/src/client.cpp" "/Users/amos/Development/Client/cmake-build-debug/CMakeFiles/Client.dir/src/client.cpp.o"
+  "/Users/amos/Development/Client/src/connectionHandler.cpp" "/Users/amos/Development/Client/cmake-build-debug/CMakeFiles/Client.dir/src/connectionHandler.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
